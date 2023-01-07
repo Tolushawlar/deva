e09b2488c61f769cbe19c79e9cc3f48c94db4906
olusola itunu <tolushaw.it@gmail.com>
2023-01-07 04:53:38 +0100

    dashboard page updated

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from "react";
+import "./Dashboard.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVoteYea, faBell } from "@fortawesome/free-solid-svg-icons";
+import HeaderImg from "../../Assets/Asset26.svg";
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <div className="dashboard">
+      <div className="DashboardCon">
+        <div className="dashLeft">
+          <div className="dashLeftImage">
+            <img src={HeaderImg} alt="headerImg" style={{ width: 200 }} />
+          </div>
+          <div className="dashLeftform">
+            <form>
+              USERID<br/><input type="text" id="userId" placeholder="1223dwfge3wd3fvbre5" />
+              <input type="text" placeholder="active" />
+              <input type="text" placeholder="accreditation" />
+            </form>
+          </div>
+        </div>
+        <div className="dashRight">
+          <div className="electionIc">
+            <div id="election">
+              <FontAwesomeIcon icon={faVoteYea} /> Election
+            </div>
+            <div id="live">live</div>
+          </div>
+          <FontAwesomeIcon icon={faBell} />
+          <div className="ringIc"></div>
+        </div>
+      </div>
+      <div className="dashImg">
+        <div className="coverDashLeft"></div>
+        <div className="coverDashRight"></div>
+      </div>
+    </div>
+  );
 }
